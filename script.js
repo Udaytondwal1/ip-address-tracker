ce413f5d1932e8cfb6b9b1539aae20b55f128c9e
Uday Tondwal <udaytondwal1@gmail.com>
2023-12-31 22:08:40 +0530

    solve apikey error

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// Fetch IP Address data from Server
-
-function Getinfo(){
-    fetch('https://geo.ipify.org/api/v2/country?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq&ipAddress=8.8.8.8')
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-   data;
-  })
-  .catch((error) => {
-    console.error(error)
-  })
-}
-
-console.log(data);
-function Setinfo(){
-
-}
 
 var map = L.map('map').setView([51.505, -0.09], 13);
 
@@ -27,4 +8,21 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('Pinch to Zoom in.<br> and Zoom Out.')
 
-
+// Fetch IP Address data from Server
+    function Getinfo(){
+      fetch('https://geo.ipify.org/api/v2/country?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq&ipAddress=8.8.8.8')
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+     data;
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  }
+  
+  console.log(data);
+  function Setinfo(){
+  
+  }
