4bf9e8fbce3601c2aec79848bece3accbcfa8cad
Uday Tondwal <udaytondwal1@gmail.com>
2023-12-31 21:50:02 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// Fetch IP Address data from Server
+
+function Getinfo(){
+    fetch('https://geo.ipify.org/api/v2/country?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq&ipAddress=8.8.8.8')
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+   data;
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+}
+
+console.log(data);
+function Setinfo(){
+
+}
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('Pinch to Zoom in.<br> and Zoom Out.')
+
+
