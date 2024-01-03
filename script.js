b7785eddfb81a93e94ea71284f3d0fc2b12c7a8a
Uday Tondwal <udaytondwal1@gmail.com>
2024-01-03 23:54:36 +0530

    successfully get clients ip address

--- script.js
+++ script.js
@@ -1,28 +1,59 @@
+// var map = L.map("map").setView([, ], 13);
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+// L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//   attribution:
+//     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+// }).addTo(map);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// L.marker([51.5, -0.09])
+//   .addTo(map)
+//   .bindPopup("Pinch to Zoom in.<br> and Zoom Out.");
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('Pinch to Zoom in.<br> and Zoom Out.')
-
-// Fetch IP Address data from Server
-    function Getinfo(){
-      fetch('https://geo.ipify.org/api/v2/country?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq&ipAddress=8.8.8.8')
-    .then((response) => {
-      return response.json()
-    })
-    .then((data) => {
-     data;
-    })
-    .catch((error) => {
-      console.error(error)
-    })
-  }
+document.querySelector("button").addEventListener("click", function () {
+  let ipaddress = "google.com"//document.querySelector(".inputf").value;
   
-  console.log(data);
-  function Setinfo(){
-  
+  if (ipaddress != 0) {
+    console.log("if run hua h")
+
+    if(ipaddress == String){
+      console.log("ye string h")
+    }else{
+    fetch(
+      "https://geo.ipify.org/api/v2/country?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq&domain=`${ipaddress}`"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        //console.log(data);
+        document.querySelector(".ipf").innerHTML = data.ip;
+        let { location } = data;
+        document.querySelector(".locationf").innerHTML = location.region;
+        document.querySelector(".tzf").innerHTML = location.timezone;
+        document.querySelector(".ispf").innerHTML = data.isp;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }
+
+  } else {
+    fetch(
+      "https://geo.ipify.org/api/v2/country?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        //console.log(data);
+        document.querySelector(".ipf").innerHTML = data.ip;
+        let { location } = data;
+        document.querySelector(".locationf").innerHTML = location.region;
+        document.querySelector(".tzf").innerHTML = location.timezone;
+        document.querySelector(".ispf").innerHTML = data.isp;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
+});
---
+++ test.js
@@ -0,0 +1,7 @@
+// TO-DO
+
+// 1. add functionality to search any ip address or domain
+
+// 2. then use those ip or domain to display loaction on map
+
+https://geo.ipify.org/api/v2/country?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq&ipAddress=8.8.8.8
