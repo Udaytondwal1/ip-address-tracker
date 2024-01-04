93bb4d623d2a840e2c7c2cf8e1d1396dc1f277c2
Uday Tondwal <udaytondwal1@gmail.com>
2024-01-04 20:21:07 +0530

    Add Functionality to search any ip address

--- script.js
+++ script.js
@@ -10,33 +10,28 @@
 //   .bindPopup("Pinch to Zoom in.<br> and Zoom Out.");
 
 document.querySelector("button").addEventListener("click", function () {
-  let ipaddress = "google.com"//document.querySelector(".inputf").value;
-  
-  if (ipaddress != 0) {
-    console.log("if run hua h")
+  let ipordomain = document.querySelector(".inputf").value;
 
-    if(ipaddress == String){
-      console.log("ye string h")
-    }else{
-    fetch(
-      "https://geo.ipify.org/api/v2/country?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq&domain=`${ipaddress}`"
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        //console.log(data);
-        document.querySelector(".ipf").innerHTML = data.ip;
-        let { location } = data;
-        document.querySelector(".locationf").innerHTML = location.region;
-        document.querySelector(".tzf").innerHTML = location.timezone;
-        document.querySelector(".ispf").innerHTML = data.isp;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-    }
-
+  if (ipordomain != 0) {
+    console.log("If Execute", ipordomain);
+      fetch(
+        "https://geo.ipify.org/api/v2/country?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq&domain=`${ipordomain}`"
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          document.querySelector(".ipf").innerHTML = data.ip;
+          let { location } = data;
+          document.querySelector(".locationf").innerHTML = location.region;
+          document.querySelector(".tzf").innerHTML = location.timezone;
+          document.querySelector(".ispf").innerHTML = data.isp;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    
   } else {
     fetch(
       "https://geo.ipify.org/api/v2/country?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq"
--- test.js
+++ test.js
@@ -4,4 +4,4 @@
 
 // 2. then use those ip or domain to display loaction on map
 
-https://geo.ipify.org/api/v2/country?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq&ipAddress=8.8.8.8
+//https://geo.ipify.org/api/v2/country?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq&ipAddress=8.8.8.8
