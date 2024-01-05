7b656062b6df2dc599f31713a793aba722abbe28
Uday Tondwal <udaytondwal1@gmail.com>
2024-01-05 19:00:34 +0530

    Added - now you can search any ip or domain

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
-// var map = L.map("map").setView([, ], 13);
+var map = L.map("map").setView([51.505, -0.09], 13);
 
-// L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-//   attribution:
-//     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-// }).addTo(map);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
 
-// L.marker([51.5, -0.09])
-//   .addTo(map)
-//   .bindPopup("Pinch to Zoom in.<br> and Zoom Out.");
 
-document.querySelector("button").addEventListener("click", function () {
+
+window.addEventListener('load', (event) => {
+    fetch(
+      "https://geo.ipify.org/api/v2/country?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        //console.log(data);
+        document.querySelector(".ipf").innerHTML = data.ip;
+        let { location } = data;
+        document.querySelector(".locationf").innerHTML = location.region;
+        document.querySelector(".tzf").innerHTML = location.timezone;
+        document.querySelector(".ispf").innerHTML = data.isp;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+
+L.marker([51.5, -0.09])
+  .addTo(map)
+  .bindPopup("Pinch to Zoom in.<br> and Zoom Out.");
+
+  document.querySelector("button").addEventListener("click", function () {
   let ipordomain = document.querySelector(".inputf").value;
 
-  if (ipordomain != 0) {
+   if (ipordomain != 0) {
     console.log("If Execute", ipordomain);
       fetch(
-        "https://geo.ipify.org/api/v2/country?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq&domain=`${ipordomain}`"
+        `https://geo.ipify.org/api/v2/country?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq&domain=` + ipordomain
       )
         .then((response) => {
           return response.json();
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // TO-DO
 
-// 1. add functionality to search any ip address or domain
+// TO-DO: 1. add functionality to search any ip address or domain ✅
 
-// 2. then use those ip or domain to display loaction on map
+// 2. then use those ip or domain to display loaction on map 
 
 //https://geo.ipify.org/api/v2/country?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq&ipAddress=8.8.8.8
