a7f7c083fbcce61df6c1e23bbc02d04856d232b1
Uday Tondwal <udaytondwal1@gmail.com>
2024-02-13 13:18:15 +0530

    Now you can search any IP address and domain

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var updateMarker;
 // Elements to Update
 let ip_field = document.querySelector(".ip-field");
 let location_field = document.querySelector(".location-field");
@@ -10,63 +9,84 @@ let ip_address = document.querySelector(".inputf").value;
 let gen_ip;
 let submit_btn = document.querySelector("button");
 
-// window load event ("when page is loaded, app automaticaly gets users IP Address and show details") 👇
 var apiUrl =
-  "https://geo.ipify.org/api/v2/country,city?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq&ipAddress=8.8.8.8";
+  "https://geo.ipify.org/api/v2/country,city?apiKey=at_iyZxRROuQlXSRuadNhDrJSS4btznp";
 
-window.addEventListener("load", initialIPDets());
-
-async function initialIPDets() {
-  var map = L.map("map").setView([51.505, -0.09], 13);
+function MapSetter(locationlat, locationlng) {
+  var map = L.map("map2").setView([locationlat, locationlng], 13);
 
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
 
-  L.marker([51.505, -0.09])
+  L.marker([locationlat, locationlng])
     .addTo(map)
     .bindPopup("Pinch to Zoom in.<br> and Zoom Out.");
+}
 
+function MapSetter2(locationlat, locationlng) {
+  var map = L.map("map").setView([locationlat, locationlng], 13);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker([locationlat, locationlng])
+    .addTo(map)
+    .bindPopup("Pinch to Zoom in.<br> and Zoom Out.");
+}
+
+async function IPdataSetter() {
   const res = await fetch(apiUrl);
-  const result = await res.json();
+  var result = await res.json();
   console.log(result);
 
   ip_field.innerHTML = result.ip;
   gen_ip = result.ip;
   console.log(gen_ip);
-  let { location } = result;
+  var { location } = result;
   location_field.innerHTML = location.region + ` ` + location.country;
   tz_field.innerHTML = `UTC ` + location.timezone;
   isp_field.innerHTML = result.isp;
-  updateMarker = [location.lat, location.lng];
+  MapSetter(location.lat, location.lng);
 }
 
-// this function provide info about any IP or domain when you click arrow button 👇
+window.addEventListener("load", () => {
+  IPdataSetter();
+});
+
+// this function provide info about any IP or domain when you type and click arrow button 👇
 
 submit_btn.addEventListener("click", () => {
   ip_address = document.querySelector(".inputf").value;
+
   if (ip_address != "") {
+    document.getElementById("map2").style.height = "0%";
+    document.getElementById("map").style.height = "100%";
     fetch(
-      `https://geo.ipify.org/api/v2/country?apiKey=at_EJ1vedab0GLZHuyMYgT3bbjP1z1dq&domain=` +
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_iyZxRROuQlXSRuadNhDrJSS4btznp&domain=` +
         ip_address
     )
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        //console.log(data);
         ip_field.innerHTML = data.ip;
         let { location } = data;
         location_field.innerHTML = location.region + ` ` + location.country;
         tz_field.innerHTML = `UTC ` + location.timezone;
         isp_field.innerHTML = data.isp;
+        MapSetter2(location.lat, location.lng);
       })
       .catch((error) => {
         alert("Unable to get IP/Domain details");
         console.log(error);
       });
   } else {
-    initialIPDets();
+    IPdataSetter();
+    document.getElementById("map2").style.height = "100%";
+    document.getElementById("map").style.height = "0%";
   }
 });
